a995ed1668c93a9da17b6fca4cc38225e79e815a
Wilson Sihombing <wilsonsihombing03@gmail.com>
2024-10-31 13:49:24 +0700

    menyelesaikan admin event

---
+++ resources/views/pages/admin/upcoming-event/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.admin')
+
+@section('content')
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">UpComing Event </h1>
+            <a href="{{ route('event-package.create') }}" class="btn btn-sm btn-primary shadow-sm">
+                <i class="fas fa-plus fa-sm text-white-50"></i> Tambah Upcoming Event
+            </a>
+        </div>
+
+        <div class="row">
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table-bordered" width="100%" collspacing="0">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Image</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            @forelse($items as $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td>{{ $item->title }}</td>
+                                    <td>{{ $item->image }}</td>
+                                    <td>
+                                        <a href="{{ route('upcoming-event.edit', $item->id) }}" class="btn btn-info">
+                                            <i class="fa fa-pencil-alt"></i>
+                                        </a>
+
+                                        <form action="{{ route('upcoming-event.destroy', $item->id) }}" method="POST"
+                                            class="d-inline">
+                                            @csrf
+                                            @method('delete')
+                                            <button class="btn btn-danger">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </form>
+                                        
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="7" class="text-center">
+                                        Data Kosong
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+    <!-- /.container-fluid -->
+@endsection
